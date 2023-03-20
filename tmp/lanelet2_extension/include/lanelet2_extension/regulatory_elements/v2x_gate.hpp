a791a94e5cbd861d690106ba5f27cdf44a97093a
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-03-20 17:49:48 +0900

    feat: add v2x gate
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ tmp/lanelet2_extension/include/lanelet2_extension/regulatory_elements/v2x_gate.hpp
@@ -0,0 +1,77 @@
+// Copyright 2023 The Autoware Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LANELET2_EXTENSION__REGULATORY_ELEMENTS__V2X_GATE_HPP_
+#define LANELET2_EXTENSION__REGULATORY_ELEMENTS__V2X_GATE_HPP_
+
+// NOLINTBEGIN(readability-identifier-naming)
+
+#include <lanelet2_core/primitives/BasicRegulatoryElements.h>
+#include <lanelet2_core/primitives/LineString.h>
+
+#include <memory>
+#include <vector>
+
+namespace lanelet::autoware
+{
+
+class V2xGate : public lanelet::RegulatoryElement
+{
+public:
+  using ConstPtr = std::shared_ptr<const V2xGate>;
+  using Ptr = std::shared_ptr<V2xGate>;
+  static constexpr char RuleName[] = "v2x_gate";
+
+  //! Directly construct a stop line from its required rule parameters.
+  //! Might modify the input data in order to get correct tags.
+  static Ptr make(Id id, const AttributeMap & attributes, const LineString3d & road_marking)
+  {
+    return Ptr{new V2xGate(id, attributes, road_marking)};
+  }
+
+  ConstLineString3d getStartLines() const;
+
+  /**
+   * @brief get the relevant road marking
+   * @return road marking
+   */
+  // [[nodiscard]] ConstLineString3d V2xGate() const;
+  // [[nodiscard]] LineString3d V2xGate();
+
+  /**
+   * @brief add a new road marking
+   * @param primitive road marking to add
+   */
+  // void setV2xGate(const LineString3d & road_marking);
+
+  /**
+   * @brief remove a road marking
+   */
+  // void removeV2xGate();
+
+private:
+  // the following lines are required so that lanelet2 can create this object
+  // when loading a map with this regulatory element
+  friend class lanelet::RegisterRegulatoryElement<V2xGate>;
+  V2xGate(Id id, const AttributeMap & attributes, const LineString3d & temp);
+  explicit V2xGate(const lanelet::RegulatoryElementDataPtr & data);
+};
+
+static lanelet::RegisterRegulatoryElement<V2xGate> reg_v2x_gate;
+
+}  // namespace lanelet::autoware
+
+// NOLINTEND(readability-identifier-naming)
+
+#endif  // LANELET2_EXTENSION__REGULATORY_ELEMENTS__V2X_GATE_HPP_
---
+++ tmp/lanelet2_extension/lib/v2x_gate.cpp
@@ -0,0 +1,89 @@
+// Copyright 2023 The Autoware Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOLINTBEGIN(readability-identifier-naming)
+
+#include "lanelet2_extension/regulatory_elements/v2x_gate.hpp"
+
+#include <boost/variant.hpp>
+
+#include <lanelet2_core/primitives/RegulatoryElement.h>
+
+#include <algorithm>
+#include <memory>
+#include <utility>
+#include <vector>
+
+namespace lanelet::autoware
+{
+namespace
+{
+
+/*
+RegulatoryElementDataPtr constructV2xGateData(
+  Id id, const AttributeMap & attributes, const LineString3d & road_marking)
+{
+  RuleParameterMap rpm;
+  RuleParameters rule_parameters = {road_marking};
+  rpm.insert(std::make_pair(RoleNameString::Refers, rule_parameters));
+
+  auto data = std::make_shared<RegulatoryElementData>(id, rpm, attributes);
+  data->attributes[AttributeName::Type] = AttributeValueString::RegulatoryElement;
+  data->attributes[AttributeName::Subtype] = "road_marking";
+  return data;
+}
+*/
+}  // namespace
+
+V2xGate::V2xGate(const RegulatoryElementDataPtr & data) : RegulatoryElement(data)
+{
+  /*
+  if (getParameters<ConstLineString3d>(RoleName::Refers).size() != 1) {
+    throw InvalidInputError("There must be exactly one road marking defined!");
+  }
+  */
+}
+
+ConstLineString3d V2xGate::getStartLines() const
+{
+  return getParameters<ConstLineString3d>("start_line").front();
+}
+
+/*
+V2xGate::V2xGate(Id id, const AttributeMap & attributes, const LineString3d & road_marking)
+: V2xGate(constructV2xGateData(id, attributes, road_marking))
+{
+}
+
+ConstLineString3d V2xGate::V2xGate() const
+{
+  return getParameters<ConstLineString3d>(RoleName::Refers).front();
+}
+
+LineString3d V2xGate::V2xGate()
+{
+  return getParameters<LineString3d>(RoleName::Refers).front();
+}
+
+void V2xGate::setV2xGate(const LineString3d & road_marking)
+{
+  parameters()[RoleName::Refers] = {road_marking};
+}
+
+void V2xGate::removeV2xGate() { parameters()[RoleName::Refers] = {}; }
+*/
+
+}  // namespace lanelet::autoware
+
+// NOLINTEND(readability-identifier-naming)
